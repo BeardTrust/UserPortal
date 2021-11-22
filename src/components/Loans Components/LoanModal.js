271ec43ea794808fc2ebe9dacc921379d0700d2e
Nathanael Grier <nathanael.grier@smoothstack.com>
2021-11-22 12:01:53 -0600

    adjust loan modal

--- src/components/Loans Components/LoanModal.js
+++ src/components/Loans Components/LoanModal.js
@@ -250,23 +250,8 @@ function LoanModal(props) {
                         Confirm Payment <GiPayMoney />
                     </Button>
                 }
-                {/* <TransactionsList assetId={props.loan.id} loan={props.loan} search={props.loan.id} /> */}
             </Modal.Body>
             <Modal.Footer>
-                {/* {pay === true &&
-                    <Button variant="secondary" onClick={handleClosePayment}>
-                        Cancel
-                    </Button>
-                }
-                {pay === false &&
-                    <Button variant="primary" onClick={handleShowPayment}>
-                        Make a Payment
-                    </Button>
-                } {pay === true && paymentAccount &&
-                    <Button variant="primary" onClick={makePayment}>
-                        Confirm Payment <GiPayMoney />
-                    </Button>
-                } */}
                 <TransactionsList assetId={props.loan.id} loan={props.loan} search={props.loan.id} />
             </Modal.Footer>
         </section>)
